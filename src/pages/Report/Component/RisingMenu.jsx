1b5af261c48642ae612c85d0d6fda8575c71a7be
DoorWinBell <msj102525@gmail.com>
2024-10-30 17:04:15 +0900

    뜨는 메뉴 ui 수정 - 문승종 20241030

--- src/pages/Report/Component/RisingMenu.jsx
+++ src/pages/Report/Component/RisingMenu.jsx
@@ -15,27 +15,27 @@ const RisingMenu = ({ risingMenuGPTData, storeInfoRedux }) => {
 
     return (
         <div className='bg-white p-4 rounded-lg shadow-md space-y-6'>
-            <p className='text-sm font-semibold'>{storeInfoRedux.detail_category_name} 매장에서 가장 많이 주문하는 메뉴?</p>
+            <p className='text-md font-semibold'>{storeInfoRedux.detail_category_name} 매장에서 가장 많이 주문하는 메뉴?</p>
             <div className="">
-                <p className='text-blue-600 text-sm font-semibold py-1'>1위
-                    <span className='text-black text-sm font-semibold'> {local_store_top5_orderd_menu.detail_category_top1_ordered_menu}</span>
+                <p className='text-blue-600 text-md font-semibold py-1'>1위
+                    <span className='text-black text-md font-semibold'> {local_store_top5_orderd_menu.detail_category_top1_ordered_menu}</span>
                 </p>
-                <p className='text-blue-600 text-sm font-semibold py-1'>2위
-                    <span className='text-black text-sm font-semibold'> {local_store_top5_orderd_menu.detail_category_top2_ordered_menu}</span>
+                <p className='text-blue-600 text-md font-semibold py-1'>2위
+                    <span className='text-black text-md font-semibold'> {local_store_top5_orderd_menu.detail_category_top2_ordered_menu}</span>
                 </p>
-                <p className='text-blue-600 text-sm font-semibold py-1'>3위
-                    <span className='text-black text-sm font-semibold'> {local_store_top5_orderd_menu.detail_category_top3_ordered_menu}</span>
+                <p className='text-blue-600 text-md font-semibold py-1'>3위
+                    <span className='text-black text-md font-semibold'> {local_store_top5_orderd_menu.detail_category_top3_ordered_menu}</span>
                 </p>
-                <p className='text-blue-600 text-sm font-semibold py-1'>4위
-                    <span className='text-black text-sm font-semibold'> {local_store_top5_orderd_menu.detail_category_top4_ordered_menu}</span>
+                <p className='text-blue-600 text-md font-semibold py-1'>4위
+                    <span className='text-black text-md font-semibold'> {local_store_top5_orderd_menu.detail_category_top4_ordered_menu}</span>
                 </p>
-                <p className='text-blue-600 text-sm font-semibold py-1'>5위
-                    <span className='text-black text-sm font-semibold'> {local_store_top5_orderd_menu.detail_category_top5_ordered_menu}</span>
+                <p className='text-blue-600 text-md font-semibold py-1'>5위
+                    <span className='text-black text-md font-semibold'> {local_store_top5_orderd_menu.detail_category_top5_ordered_menu}</span>
                 </p>
 
                 <div className="">
                     <p className='font-bold py-4'>백쉐프의 조언 들어보세요~</p>
-                    <p className='text-sm'>{rising_menu_advice}</p>
+                    <p className='text-md'>{rising_menu_advice}</p>
                 </div>
             </div>
         </div>
