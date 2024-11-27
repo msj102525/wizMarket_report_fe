e27b67164a68185d8f2282ad2a57f7863fc5517d
DoorWinBell <msj102525@gmail.com>
2024-11-27 14:00:42 +0900

    필요없는 페이지 삭제 - 문승종 20241127

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main/Main";
 import Report from "./pages/Report/Report";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
         <Route path="/wizmarket/report/:store_business_id" element={<Report />} />
       </Routes>
     </BrowserRouter>
